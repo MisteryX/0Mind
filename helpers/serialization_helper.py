9369e75ec0010f69b942707d0e4bf3fb11efa9a9
Maxim Morskov <xxxa0c@mail.ru>
2018-05-17 20:16:21 +0300

    1. SerializationHelper appended with unified save_model method and caffe2 serialization support 2. Documentation refactoring (started work on sklearn docs)

--- helpers/serialization_helper.py
+++ helpers/serialization_helper.py
@@ -13,6 +13,12 @@ try:
 except ImportError:
 	pass
 
+try:
+	from caffe2.proto import caffe2_pb2
+	from caffe2.python import workspace, utils, core
+except ImportError:
+	pass
+
 import json
 import os
 from helpers.file_helper import FileHelper
@@ -42,6 +48,13 @@ class SerializationHelper:
 	}
 
 	@staticmethod
+	def save_model(model_type: str, model, file_name, inputs_spec: dict, output_spec: dict):
+		if 'sklearn' == model_type:
+			SerializationHelper.build_sklearn_model_file(model, file_name, inputs_spec, output_spec)
+		elif 'caffe2' == model_type:
+			SerializationHelper.build_caffe2_model_file(model, file_name, inputs_spec, output_spec)
+
+	@staticmethod
 	def get_sklearn_model_from_file(model_file_content):
 		return joblib.load(model_file_content)
 
@@ -55,6 +68,43 @@ class SerializationHelper:
 			SerializationHelper.get_list_of_model_file_content('sklearn')
 		)
 		os.remove(SKLEARN_MODEL_FILE_NAME)
+		os.remove(INPUT_SPEC_FILE_NAME)
+		os.remove(OUTPUT_SPEC_FILE_NAME)
+
+	@staticmethod
+	def build_caffe2_model_file(model, file_name, inputs_spec: dict, output_spec: dict):
+		with open(CAFFE2_MODEL_PREDICT_FILE_NAME, 'wb') as f:
+			f.write(model.net._net.SerializeToString())
+		init_net = caffe2_pb2.NetDef()
+		for param in model.params:
+			op = core.CreateOperator(
+				"GivenTensorFill",
+				[],
+				[param],
+				arg=[
+					utils.MakeArgument("shape", workspace.FetchBlob(param).shape),
+					utils.MakeArgument("values", workspace.FetchBlob(param))
+				]
+			)
+			init_net.op.extend([op])
+		init_net.op.extend([core.CreateOperator(
+				"ConstantFill",
+				[],
+				[inputs_spec['inputs']['0']['name']],
+				shape=tuple(inputs_spec['inputs']['0']['shape'])
+			)]
+		)
+		with open(CAFFE2_MODEL_INIT_FILE_NAME, 'wb') as f:
+			f.write(init_net.SerializeToString())
+
+		FileHelper.write_to_file(INPUT_SPEC_FILE_NAME, json.dumps(inputs_spec))
+		FileHelper.write_to_file(OUTPUT_SPEC_FILE_NAME, json.dumps(output_spec))
+		FileHelper.write_files_to_tar(
+			file_name + '.caffe2',
+			SerializationHelper.get_list_of_model_file_content('caffe2')
+		)
+		os.remove(CAFFE2_MODEL_INIT_FILE_NAME)
+		os.remove(CAFFE2_MODEL_PREDICT_FILE_NAME)
 		os.remove(INPUT_SPEC_FILE_NAME)
 		os.remove(OUTPUT_SPEC_FILE_NAME)
 
